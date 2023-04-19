c2ad5528bb248c45bbadf057f5d6011627979779
unknown <estebanavella1@gmail.com>
2023-04-19 19:06:07 -0300

    desesperado 1.8

--- src/main/java/com/backend/backendAvella/Controller/SkillController.java
+++ src/main/java/com/backend/backendAvella/Controller/SkillController.java
@@ -5,19 +5,16 @@ import com.backend.backendAvella.Entity.Skill;
 import com.backend.backendAvella.Interface.ISkillsService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@CrossOrigin( origins = "https://portfolio-avella.web.app" )
 
 public class SkillController {
 
