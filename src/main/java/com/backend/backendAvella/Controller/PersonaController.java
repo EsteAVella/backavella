9779410dc55d35f191379476693f82e04f0b6dbd
unknown <estebanavella1@gmail.com>
2023-04-22 00:44:14 -0300

    desesperacion 3.3

--- src/main/java/com/backend/backendAvella/Controller/PersonaController.java
+++ src/main/java/com/backend/backendAvella/Controller/PersonaController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin( origins = "https://portfolio-avella.web.app" )
+@CrossOrigin(origins="https://portfolio-avella.web.app")
                          
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
--- src/main/java/com/backend/backendAvella/Entity/Educacion.java
+++ src/main/java/com/backend/backendAvella/Entity/Educacion.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.backend.backendAvella.Entity;
 
 
@@ -19,6 +16,7 @@ import lombok.Setter;
 
 @Getter @Setter
 @Entity
+
 public class Educacion implements Serializable {
     
     @Id
